cea5cde7d10566e59de2b30032336c6cbea5b4f8
1nchy <2493242629@qq.com>
2021-05-26 10:14:54 +0800

    debug

--- display.h
+++ display.h
@@ -13,7 +13,7 @@ using std::cout;
 using std::endl;
 
 const string welcomeMsg = "Calculator by 1nchy, enjoy yourself ~";
-const string versionMsg = "Version 3.2.3 on linux";
+const string versionMsg = "Version 3.2.4 on linux";
 const string inputNotice = ">>> ";
 const char asgnCmd = ':';
 const char funcCmd = '\\';
